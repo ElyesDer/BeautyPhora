c1ef91eb65353103472189997a66bf0f31a3c6aa
Elyes <lyesderouich@gmail.com>
2023-01-12 15:33:00 +0100

    Initial Commit

---
+++ BeautyPhora/Application/BeautyPhoraApp.swift
@@ -0,0 +1,20 @@
+//
+//  BeautyPhoraApp.swift
+//  BeautyPhora
+//
+//  Created by Derouiche Elyes on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeautyPhoraApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
